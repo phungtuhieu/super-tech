21c7c878b52d80ce032a44f2bddecc23f0fb785d
phungtuhieu <hieu.phung@paptech.co>
2024-08-31 11:04:32 +0700

    init spring boot project

---
+++ be-springboot-supertech/src/main/java/com/web/be_springboot_supertech/BeSpringbootSupertechApplication.java
@@ -0,0 +1,13 @@
+package com.web.be_springboot_supertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringbootSupertechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringbootSupertechApplication.class, args);
+	}
+
+}
---
+++ be-springboot-supertech/src/test/java/com/web/be_springboot_supertech/BeSpringbootSupertechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.be_springboot_supertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringbootSupertechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
