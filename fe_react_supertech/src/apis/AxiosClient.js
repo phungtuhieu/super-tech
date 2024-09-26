3ce8b26f5b555b1fcf4001f3e2d695f977ca52cc
phungtuhieu <hieu.phung@paptech.co>
2024-09-26 14:17:36 +0700

    install env

--- fe_react_supertech/src/apis/AxiosClient.js
+++ fe_react_supertech/src/apis/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const config = {
-  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL
+  baseURL: import.meta.env.VITE_BASE_URL_API
 }
 
 const AxiosClient = axios.create(config);
